59d68e0efa5d355909d03298208a97e775fd23ce
André <redan.blue27@gmail.com>
2017-06-11 00:34:42 +0100

    Added a generic bounded stack implementation

---
+++ Stack.c
@@ -0,0 +1,57 @@
+#include "Stack.h"
+
+struct stack {
+    void *data;
+    int size;
+    size_t elem_size;
+    size_t stack_max;
+};
+
+
+
+Stack create_stack(const size_t elem_size,const int stack_max_cap)
+{
+    Stack s = malloc (sizeof *s);
+    if (s)
+    {
+      s->elem_size=elem_size;
+      s->size = 0;
+      s->stack_max=stack_max_cap;
+      s->data = malloc( s->elem_size*s->stack_max);
+      if(!s->data)
+      {
+        free(s);
+        s=NULL;
+      }
+    }
+    return s;
+}
+
+void *top(const Stack s)
+{
+    if (s->size == 0) {
+        fprintf(stderr, "Error: stack empty\n");
+        return -1;
+    }
+
+    return s->data[s->size-1*s->elem_size];
+}
+
+void push(Stack s, void *elem)
+{
+    if (s->size < stack_max_cap)
+    {
+      memcpy(s->data+(s->size*s->elem_size),elem,s->elem_size);
+      s->size++;
+    }
+    else
+        fprintf(stderr, "Error: stack full\n");
+}
+
+void pop(const Stack s)
+{
+    if (s->size == 0)
+        fprintf(stderr, "Error: stack empty\n");
+    else
+        s->size--;
+}
